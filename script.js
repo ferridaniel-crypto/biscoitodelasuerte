59d8253c238eb63a3c09820964453c0573c877b0
ferridaniel-crypto <121793237+ferridaniel-crypto@users.noreply.github.com>
2023-06-21 05:29:29 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let screen1;
+
+function sorte (){
+    let screen1 = screen1.classList.toggle('hide')
+}
+    
+butao1.addEventListener("click", function(sorte){
+let screen1 = screen1.classList.toggle('hide')
+});
+
+
